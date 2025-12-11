89c25d4fffb7e1f5e2edb1880739af24fc6fe0ab
Andrey Radilov <a.radilov@adtelligent.com>
2025-12-11 16:01:20 +0200

    init

---
+++ uniqid.go
@@ -0,0 +1,64 @@
+package uniqid
+
+import (
+	"log"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var serverID uint16
+
+var once sync.Once
+
+// SetServerID sets the serverID to the provided value if it has not already been set; panics if serverID is already set.
+func SetServerID(id uint16) {
+	if serverID > 0 {
+		log.Panicf("serverID already set")
+	}
+	serverID = id
+}
+
+// initServerID initializes the serverID using the external IP, setting it based on the last two bytes of the IP address.
+func initServerID() {
+	if serverID > 0 {
+		return
+	}
+	ip4 := ExternalIP().To4()
+	if ip4 == nil {
+		log.Panicf("cannot get external ip")
+	}
+
+	serverID = uint16(ip4[2])<<8 | uint16(ip4[3])
+}
+
+var uniqueAdID = func() uint64 {
+	return uint64(time.Now().UnixNano())
+}()
+
+// Get generates a globally unique 64-bit identifier combining a server-specific ID and an atomic counter.
+func Get() uint64 {
+	once.Do(initServerID)
+	adID := atomic.AddUint64(&uniqueAdID, 1)
+	const mask48 uint64 = (uint64(1) << 48) - 1
+	return (uint64(serverID) << 48) | (adID & mask48)
+}
+
+// Append appends unique id hex to dst.
+func Append(dst []byte) []byte {
+	n := Get()
+
+	for i := uint(1); i <= 8; i++ {
+		shift := 64 - (i << 3)
+		c := byte(n >> shift)
+		dst = append(dst, hexByte(c>>4), hexByte(c&0xf))
+	}
+	return dst
+}
+
+func hexByte(c byte) byte {
+	if c < 10 {
+		return '0' + c
+	}
+	return c - 10 + 'A'
+}
